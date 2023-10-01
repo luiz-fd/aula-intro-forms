dec673b105078b9a5ccf62902681a0bf580514de
Luiz F Dias <luizfd@gmail.com>
2023-10-01 08:14:46 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+type FormData = {
+  firstName: string;
+  lastName: string;
+}
+
+function App() {
+
+  const [fullName, setFullName] = useState();
+  const [formData, setFormData] = useState<FormData>({
+    firstName:'',
+    lastName: ''
+  });
+
+  function handleInputChange(event: any){
+    const value = event.target.value;
+    const name = event.target.name;
+    setFormData({...formData, [name]:value })
+  }
+
+  function handleFormSubmit(event: any){
+event.preventDefault();
+setFullName(formData.firstName + " " + formData.lastName);
+  }
+
+  return (
+    <>
+      <form onSubmit={handleFormSubmit}>
+        <div>
+          <input
+          name="firstName"
+          value={formData.firstName}
+           type="text" 
+           placeholder="Digite seu nome" 
+           onChange={handleInputChange}
+           />
+        </div>
+        <div>
+          <input 
+          name="lastName"
+          value={formData.lastName}
+          type="text"
+           placeholder="Digite o sobrenome" 
+           onChange={handleInputChange}
+           />
+        </div>
+        <div>
+          <button type="submit">Mostrar nome completo</button>
+        </div>
+      </form>
+      <h2>{fullName }</h2>
+    </>
+  );
+}
+
+export default App;
